00e216bc1d5481b55a6dc437402a83700cbaa58c
uniquetraker <uniquetraker@gmail.com>
2024-10-01 09:38:59 +0530

    file changes

--- server/index.js
+++ server/index.js
@@ -15,7 +15,13 @@ import { pendingProductRouter } from "./routers/pendingProductRouter.js";
 import { notificationRouter } from "./routers/notificationRouter.js";
 import { pendingCategoryRouter } from "./routers/pendingCategoryRouter.js";
 import { ContactRouter } from "./routers/contactRouter.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
+const _filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(_filename);
+
+// console.log("dirname:",__dirname)
 dotenv.config();
 const app = express();
 
@@ -27,13 +33,18 @@ const allowdDomains =
     ? [process.env.REMOTE_CLIENT_APP, process.env.REMOTE_SERVER_API] // Corrected here
     : [process.env.LOCAL_CLIENT_APP, process.env.LOCAL_SERVER_API];
 
-    app.use(cors({
-      origin: process.env.NODE_ENV === "production" 
-          ? process.env.REMOTE_CLIENT_APP 
-          : process.env.LOCAL_CLIENT_APP,
-      credentials: true // Optional: If you want to include cookies in requests
-  }));
-  
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? process.env.REMOTE_CLIENT_APP
+        : process.env.LOCAL_CLIENT_APP,
+    credentials: true, // Optional: If you want to include cookies in requests
+  })
+);
+
+console.log(allowdDomains);
+
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
@@ -50,7 +61,12 @@ app.use("/api", notificationRouter);
 app.use("/api", pendingCategoryRouter);
 app.use("/api", ContactRouter);
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => 
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"))
+);
 // Correct PORT usage
 const PORT = process.env.port || 3000; // Fallback to 3000 locally
 
-app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
